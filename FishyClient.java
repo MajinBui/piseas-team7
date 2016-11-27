f0c3f778ca8e753e3d0415dcda7a7baacce6bb62
MajinBui <vanchau28@gmail.com>
2016-11-27 04:54:53 -0500

    added more comments

--- FishyClient.java
+++ FishyClient.java
@@ -14,18 +14,17 @@ public class FishyClient {
     	String tankID = "QWERT"; // To access the saved data, its is at vanchaubui.com/fish_tanks/<tankID>.html
 
     	//	how to recieve data
-//        HashMap<String, String> dataList = FishyClient.retrieveServerData(tankID);
-//        Iterator it = dataList.entrySet().iterator();
-//        while (it.hasNext()) {
-//        	HashMap.Entry pair = (HashMap.Entry)it.next();
-//            System.out.println(pair.getKey() + " = " + pair.getValue());
-//            it.remove(); // avoids a ConcurrentModificationException
-//        }
+        HashMap<String, String> dataList = FishyClient.retrieveServerData(tankID);
+        Iterator it = dataList.entrySet().iterator();
+        while (it.hasNext()) {
+        	HashMap.Entry pair = (HashMap.Entry)it.next();
+            System.out.println(pair.getKey() + " = " + pair.getValue());
+            it.remove(); // avoids a ConcurrentModificationException
+        }
         //	how to send server data
-
-    	HashMap<String, String> dataList = new HashMap<String, String>();
+    	dataList = new HashMap<String, String>();
         dataList.put("tankId", tankID);
-        dataList.put("ph Level", "5");
+        dataList.put("ph Level", "6");
         dataList.put("water conducitivty level", "30000");
         dataList.put("temperature", "23 degrees");
         dataList.put("feeding time", "7:00pm");
@@ -34,11 +33,22 @@ public class FishyClient {
         FishyClient.writeToServerData(tankID, dataList);
         
 	}
+	/**
+	 * Connects to the server
+	 * @return Socket to the server
+	 * @throws UnknownHostException Happens when I forget to pay for server.  Panic
+	 * @throws IOException Panic
+	 */
 	private static Socket connectToServer() throws UnknownHostException, IOException  {
 		InetAddress addr = InetAddress.getByName("198.27.80.59"); //"198.27.80.59" is vanchaubui.com
 		Socket clientSocket = new Socket(addr, 4444);
         return clientSocket;
 	}
+	/**
+	 * Returns the current saved data of the given TankID as a HashMap<String, String>
+	 * @param tankId the id of the tank to retrieve data from
+	 * @return a HashMap<String, String> of the server data stored
+	 */
 	public static HashMap<String, String> retrieveServerData(String tankId) {
 		try {
 			Socket clientSocket = connectToServer();
@@ -65,7 +75,11 @@ public class FishyClient {
 		System.err.println("no data found!");
 		return new HashMap<String, String>();
 	}
-	
+	/**
+	 * Updates the tank with the given TankId with the HashMap<String, String> given
+	 * @param tankId the TankID of the tank that needs updating
+	 * @param dataHashMap the HashMap<String, String> that the tank's save data will be updated to
+	 */
 	public static void writeToServerData(String tankId, HashMap<String, String> dataHashMap) {
 		try {
 			Socket clientSocket = connectToServer();
