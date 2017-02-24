2f421fa0004eed6556ffb76329c95b77792a8e7f
MajinBui <vanchau28@gmail.com>
2017-02-23 19:37:10 -0500

    moved

--- Pins.h
+++
@@ -1,7 +0,0 @@
-#ifndef _PINS_
-#define _PINS_
-
-const int LIGHT_PIN = 26;
-const int FAN_PIN = 19;
-
-#endif
--- xmlParser.h
+++
@@ -1,7 +0,0 @@
-#include "Tank.h"
-
-class XmlParser {
-public:
-	static void updateLightSchedule(LightSchedule&);
-	static void updateTemperatureRange(TempData&);
-};
